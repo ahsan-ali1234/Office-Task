445d68d4499ac6d168297ece13d4a493eb90ee0f
ahsan-ali1234 <89576608+ahsan-ali1234@users.noreply.github.com>
2023-07-06 18:01:43 +0500

    Update my_test.py
    second task

--- my_test.py
+++ my_test.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2023, me and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class mytest(Document):
-	@frappe.whitelist()
-def get_customers():
-   def myy_method(doctype, txt, searchfield, start, page_len, filters):
-    return froggy.db.sql('''SELECT name FROM `tabSupplier` WHERE Supplier Group= 'distributor' ''')
+    pass
+
+@frappe.whitelist()
+def supplier(supplier_name):
+    supplier_doc = frappe.get_doc('Supplier', supplier_name)
+    country = supplier_doc.country
+
+    return country
